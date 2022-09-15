0f4fd85ad9871b08c1bcce48eda7c8b2424f7777
Sampangi Charan Rajeev <108627841+Charanrajeev@users.noreply.github.com>
2022-09-16 00:02:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import react from 'react'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 data-ns-test="Ecommerse Website">Ecommerse Website</h1>
+      <h2 data-ns-test="it is the site where you can do all shopping activites in fast manner">it is the site where you can do all shopping activites in fast manner</h2>
+    </div>
+  );
+}
